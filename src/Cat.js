f1ac97c6118002e845ec28a7c2b5464cf8a7b12a
SheaClose <SheaClose@gmail.com>
2018-06-01 12:47:30 -0500

    done

---
+++ src/Cat.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cat = () => {
+  return (
+    <div>
+      <h1>Hello From Cat</h1>
+    </div>
+  );
+};
+
+export default Cat;
---
+++ src/Dog.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Dog extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Hello from Dog</div>
+        <div>I dog!</div>
+      </div>
+    );
+  }
+}
+
+export default Dog;
